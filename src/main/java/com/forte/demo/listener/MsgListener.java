fa2a19bc6ab94d080f38a2f624215908536beb03
LiChen <dcam00r0@qq.com>
2019-08-26 23:17:17 +0800

    多加了几个注解

--- src/main/java/com/forte/demo/listener/MsgListener.java
+++ src/main/java/com/forte/demo/listener/MsgListener.java
@@ -28,30 +28,43 @@ public class MsgListener {
      * 字段所使用的@Depend注解为在本框架中使用
      * 而@Autowired则在Spring中被使用
      * 请注意不要搞混了
+     *
+     * 这个mapper是群主写的一个访问数据库的类，用不到
      * */
     @Depend
     private MessageMapper mapper;
 
+    /**
+     * 腾讯ai的id和key
+     */
     public static final String APP_ID = "2121061546";
     public static final String APP_KEY = "hTAA8RaUJj0qipEA";
 
 
-
+    /**
+     * 这个是监听私聊的方法，@Listen注解括号里的是枚举，专门用来监听私聊消息的
+     * @param privateMsg 这个是私聊过来的消息
+     * @param sender 这个是发送数据的
+     * @throws Exception
+     */
     @Listen(MsgGetTypes.privateMsg)
     public void priMsg(PrivateMsg privateMsg, MsgSender sender) throws Exception {
         //记录消息
         String msg = privateMsg.getMsg();
-        TAipNlp aipNlp = new TAipNlp(APP_ID, APP_KEY);
-        String session = System.currentTimeMillis()/1000+"";
 
         //基于腾讯ai的基础闲聊
+        TAipNlp aipNlp = new TAipNlp(APP_ID, APP_KEY);
+        //session是秒级的时间戳
+        String session = System.currentTimeMillis()/1000+"";
         String result = aipNlp.nlpTextchat(session,msg);
 
+        //获取来的json可以输出看看
         JSONObject json = JSON.parseObject(result);
+        //我这里直接拿到ai的回答了
         JSONObject data = (JSONObject) json.get("data");
         String answer = (String) data.get("answer");
+        //发送数据，两个参数一个QQ号一个文本
         sender.SENDER.sendPrivateMsg(privateMsg.getQQ(),answer);
     }
-
 
 }

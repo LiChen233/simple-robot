3d2c9ea101ad8fcfdc073619a4035636c90702df
LiChen <dcam00r0@qq.com>
2019-08-28 22:56:18 +0800

    合成图片写了个小demo

--- src/main/java/test/ImageTest.java
+++ src/main/java/test/ImageTest.java
@@ -2,10 +2,40 @@ package test;
 
 import org.junit.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ImageTest {
 
     @Test
-    public void test1(){
+    public void test1() throws IOException {
+        BufferedImage thumbImage = new BufferedImage(1202, 1700, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = thumbImage.createGraphics();
 
+        File file = new File("C:\\Users\\Chen\\Desktop\\666.jpg");
+        Image src = javax.imageio.ImageIO.read(file);
+        g.drawImage(src.getScaledInstance(1202,1700,Image.SCALE_SMOOTH), 0, 0, null);
+
+        File file2 = new File("C:\\Users\\Chen\\Desktop\\touxiang.png");
+        Image img = javax.imageio.ImageIO.read(file2);
+        g.drawImage(img.getScaledInstance(484,467,Image.SCALE_SMOOTH), 0, 0, null);
+
+        String s="我要加的水印";
+        g.setColor(Color.RED);
+        g.setFont(new Font("宋体",Font.PLAIN,150));
+        g.drawString(s,0,300);
+        g.dispose();
+
+        String path = "C:\\Users\\Chen\\Desktop\\123.jpg";
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(path));
+        String formatName = path.substring(path.lastIndexOf(".") + 1);
+        ImageIO.write(thumbImage, /*"GIF"*/ formatName /* format desired */ , new File(path) /* target */ );
+
+        out.close();
     }
 }

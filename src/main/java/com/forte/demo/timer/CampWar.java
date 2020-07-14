526ab11167259c30231bc56a2f20716c82f1af00
LC <dcam00r0@qq.com>
2020-07-14 15:14:52 +0800

    阵营战提醒

--- src/main/java/com/forte/demo/timer/CampWar.java
+++ src/main/java/com/forte/demo/timer/CampWar.java
@@ -1,9 +1,14 @@
 package com.forte.demo.timer;
 
+import com.forte.demo.MainApplication;
+import com.forte.demo.bean.QqGroup;
+import com.forte.demo.dao.QqGroupDao;
 import com.forte.qqrobot.anno.timetask.CronTask;
 import com.forte.qqrobot.sender.MsgSender;
 import com.forte.qqrobot.timetask.TimeJob;
 import com.forte.qqrobot.utils.CQCodeUtil;
+
+import java.util.ArrayList;
 
 /**
  * 阵营战提醒
@@ -13,6 +18,10 @@ public class CampWar implements TimeJob {
 
     @Override
     public void execute(MsgSender msgSender, CQCodeUtil cqCodeUtil) {
-
+        QqGroupDao qqGroupDao = MainApplication.depends.get(QqGroupDao.class);
+        ArrayList<QqGroup> allGroup = qqGroupDao.getAllGroup();
+        allGroup.forEach(e -> {
+            msgSender.SENDER.sendGroupMsg(e.getGroupid(),"争夺战将于19:00开始，请各位做好准备");
+        });
     }
 }
